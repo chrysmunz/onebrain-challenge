4c2a55fac6be6ad9881b87044806aab2dfbf4ec6
chrysmunz <chrys.munz@hotmail.com>
2021-12-08 03:56:29 -0300

    feat: :sparkles: new button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import { Title, Type1, Type2 } from './styles';
+
+const Button = (props) => {
+  if (props.type === 'default') {
+    return (
+      <Type1 onPress={props.onPress}>
+        <Title>{props.children}</Title>
+      </Type1>
+    );
+  }
+
+  return (
+    <Type2 onPress={props.onPress}>
+      {props.type === 'more'
+        ? <Icon name='plus' size={30} color='#333333' />
+        : props.type === 'less'
+          ? <Icon name='minus' size={30} color='#333333' />
+          : <Icon name='history' size={30} color='#333333' />
+      }
+    </Type2>
+  );
+}
+export default Button;
---
+++ src/components/Button/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+
+export const Type1 = styled.TouchableOpacity`
+  background-color: #D8D8D8;
+  width: 100%;
+  align-items: center;
+  border-radius: 30px;
+  justify-content: center;
+  height: 48px;
+  margin-bottom: 20px;
+`;
+
+export const Type2 = styled.TouchableOpacity`
+  background-color: #D8D8D8;
+  align-items: center;
+  border-radius: 30px;
+  justify-content: center;
+  height: 48px;
+  width: 48px;
+`;
+
+export const Title = styled.Text`
+  color: #333333;
+  text-align: center;
+  font-weight: bold;
+  font-size: 18px;
+`;
