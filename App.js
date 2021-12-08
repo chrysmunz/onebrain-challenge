9820610bc8965c1ce644385de7ca6d8aaeb2e9b1
chrysmunz <chrys.munz@hotmail.com>
2021-12-08 04:39:57 -0300

    feat: :sparkles: toast interaction

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { Provider } from 'react-redux';
+import Toast from 'react-native-toast-message';
 
 import Router from './src/router/Router';
 import store from './src/store';
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <Router />
+      <Toast />
     </Provider>
   );
 }
--- src/screens/Config/index.js
+++ src/screens/Config/index.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Toast from 'react-native-toast-message';
 
 import { add, decrement, increment, remove, reset } from '../../actions/counter';
 import { Container, Controller } from './styles';
@@ -10,10 +11,33 @@ const Config = () => {
 	const counter = useSelector((state) => state.counter);
 	const dispatch = useDispatch();
 
+	const toastAdd = () => {
+    Toast.show({
+      type: 'success',
+      text1: 'counter successfully added'
+    });
+  };
+
+	const toastRemove = () => {
+    Toast.show({
+      type: 'success',
+      text1: `counter ${counter.selected} successfully removed`,
+    });
+  };
+	
+	const addCounter = async () => {
+		dispatch(add());
+		toastAdd();
+	};
+
+	const removeCounter = async () => {
+		dispatch(remove(counter.selected));
+		toastRemove();
+	};
+
 	return (
 		<Container>
-			<Button type='default' onPress={() => dispatch(add())}>Add</Button>
-			<Button type='default' onPress={() => dispatch(remove(counter.selected))}>Remove</Button>
+			<Button type='default' onPress={() => addCounter()}>Add new counter</Button>
 			{counter.selected ?
 				<>
 					<Controller>
@@ -25,6 +49,7 @@ const Config = () => {
 						selected={counter.selected}
 						data={counter.counters.filter(item => item.id === counter.selected)[0]}
 					/>
+					<Button type='default' onPress={() => removeCounter()}>Remove</Button>
 				</>
 				: null
 			}
