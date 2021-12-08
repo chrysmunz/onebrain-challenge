5f9dcbe656de8d70acb8def2d00ee2d7191f0eef
chrysmunz <chrys.munz@hotmail.com>
2021-12-08 09:49:06 -0300

    feat: :sparkles: added empty state

--- src/screens/Counters/index.js
+++ src/screens/Counters/index.js
@@ -1,31 +1,38 @@
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { select } from '../../actions/counter';
-import { FlatList } from './styles.js';
+import { Container, Empty, FlatList, Text, Title } from './styles.js';
 import { Counter } from '../../components';
 
 const Counters = () => {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    console.log(counter.selected);
-  }, []);
-
   return (
-    <FlatList
-      data={counter.counters}
-      keyExtractor={(_, index) => index.toString()}
-      renderItem={({ item }) => (
-        <Counter
-          data={item}
-          selected={counter.selected}
-          onPress={() => dispatch(select(item.id))}
+    <Container>
+      {counter.counters.length === 0 ?
+       <Empty>
+         <Icon name='list-alt' size={60} color='#FFFFFF' />
+         <Title>Empty list</Title>
+         <Text>You have no counters at the moment</Text>
+       </Empty> :
+        <FlatList
+          data={counter.counters}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Counter
+              data={item}
+              selected={counter.selected}
+              onPress={() => dispatch(select(item.id))}
+            />
+          )}
         />
-      )}
-    />
+      }
+
+    </Container>
   );
 }
 
--- src/screens/Counters/styles.js
+++ src/screens/Counters/styles.js
@@ -1,8 +1,34 @@
 import styled from 'styled-components/native';
 
+export const Container = styled.View`
+  flex: 1;
+  background-color: #406377;
+  padding-horizontal: 10%;
+  justify-content: center;
+`;
+
+export const Empty = styled.View`
+  flex: 1;
+  background-color: #406377;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+export const Text = styled.Text`
+  color: #A9A9A9;
+  text-align: center;
+  font-size: 16px;
+  font-weight: normal;
+  padding-horizontal: 20%;
+`;
+
 export const FlatList = styled.FlatList`
   flex: 1;
-  background-color: #406377
-  padding-top: 20px;
-  padding-horizontal: 10%;
 `;
