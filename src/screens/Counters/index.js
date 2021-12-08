8b1db76e8ef0aa76bf776ccae64080c2330db21a
chrysmunz <chrys.munz@hotmail.com>
2021-12-08 03:59:26 -0300

    feat: :sparkles: counters screen

--- src/screens/Counters/index.js
+++ src/screens/Counters/index.js
@@ -1,8 +1,32 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { select } from '../../actions/counter';
+import { FlatList } from './styles.js';
+import { Counter } from '../../components';
 
 const Counters = () => {
-  return null;
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(counter.selected);
+  }, []);
+
+  return (
+    <FlatList
+      data={counter.counters}
+      keyExtractor={(_, index) => index.toString()}
+      renderItem={({ item }) => (
+        <Counter
+          data={item}
+          selected={counter.selected}
+          onPress={() => dispatch(select(item.id))}
+        />
+      )}
+    />
+  );
 }
 
 export default Counters;
---
+++ src/screens/Counters/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const FlatList = styled.FlatList`
+  flex: 1;
+  background-color: #406377
+  padding-top: 20px;
+  padding-horizontal: 10%;
+`;
