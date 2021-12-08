1d361fb6974043a903b180bac16763e03950fe7b
chrysmunz <chrys.munz@hotmail.com>
2021-12-08 03:59:52 -0300

    feat: :sparkles: config screen

--- src/screens/Config/index.js
+++ src/screens/Config/index.js
@@ -1,8 +1,36 @@
 
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { add, decrement, increment, remove, reset } from '../../actions/counter';
+import { Container, Controller } from './styles';
+import { Button, Counter } from '../../components';
 
 const Config = () => {
-  return null;
+	const counter = useSelector((state) => state.counter);
+	const dispatch = useDispatch();
+
+	return (
+		<Container>
+			<Button type='default' onPress={() => dispatch(add())}>Add</Button>
+			<Button type='default' onPress={() => dispatch(remove(counter.selected))}>Remove</Button>
+			{counter.selected ?
+				<>
+					<Controller>
+						<Button type='less' onPress={() => dispatch(decrement(counter.selected))} />
+						<Button type='reset' onPress={() => dispatch(reset(counter.selected))} />
+						<Button type='more' onPress={() => dispatch(increment(counter.selected))} />
+					</Controller>
+					<Counter
+						selected={counter.selected}
+						data={counter.counters.filter(item => item.id === counter.selected)[0]}
+					/>
+				</>
+				: null
+			}
+
+		</Container>
+	);
 }
 
 export default Config;
---
+++ src/screens/Config/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  background-color: #406377
+  padding-vertical: 20px;
+  padding-horizontal: 10%;
+`;
+
+export const Controller = styled.View`
+  flex-direction: row;
+	justify-content: space-between;
+	margin-top: 60px;
+  margin-bottom: 20px;
+`;
